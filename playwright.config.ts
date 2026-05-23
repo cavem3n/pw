e2827393cfdc3e6778eead257459404f7526590d
Felipe <felipelpv96@gmail.com>
2026-05-23 16:01:23 -0500

    lastlast

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   /* workers: process.env.CI ? 1 : undefined, */
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['list'],['allure-playwright', { outputFolder: 'allure-results' }]],
+  reporter: [['line'],['allure-playwright', { resultsDir: './allure-results' ,  outputFolder: './allure-results'}]],
   timeout: 5_000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
