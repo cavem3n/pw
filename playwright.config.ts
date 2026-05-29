910f61802f63f57ebc508f9e558264b5af694204
Felipe <felipelpv96@gmail.com>
2026-05-29 15:15:25 -0500

    YmlFix2

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: false,
-  workers: process.env.CI ? 1 : undefined
+  workers: process.env.CI ? 1 : undefined,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
